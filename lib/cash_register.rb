a4d80602273176a54a0fdf391a06d43858e8927c
Daniel Bovington <d.bovington@gmail.com>
2019-07-16 14:37:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,23 +4,24 @@ class CashRegister
   attr_accessor :discount, :purchases, :total, :transaction
 
   def initialize(discount=nil)
-    self.total = 0
-    self.discount = discount if discount != nil
-    self.purchases = []
+    @total = 0
+    @discount = discount if discount != nil
+    @purchases = []
+    @transaction = []
   end
 
   def add_item(name, price, quantity=1)
-    self.transaction = [name, price, quantity]
+    @transaction << [name, price, quantity]
     quantity.times do
-      self.purchases.push self.transaction[0]
-      self.total += self.transaction[1]
+      @purchases.push @transaction.last[0]
+      @total += @transaction.last[1]
     end
   end
 
   def apply_discount
     if discount
-      self.total = self.total - self.total*self.discount/100
-      result = "After the discount, the total comes to $#{self.total}."
+      @total = @total - @total * @discount / 100
+      result = "After the discount, the total comes to $#{@total}."
     else
       result = "There is no discount to apply."
     end
@@ -28,15 +29,28 @@ class CashRegister
   end
 
   def items
-    return self.purchases
+    return @purchases
   end
 
   def void_last_transaction
-    quantity = self.transaction[2]
+    last_transaction = @transaction.pop
+    quantity = last_transaction[2]
     quantity.times do
-      self.purchases.pop
-      self.total -= self.transaction[1]
+      @purchases.pop
+      @total -= last_transaction[1]
     end
   end
 
 end
+
+new = CashRegister.new
+new.add_item("crisps", 0.50, 2)
+new.add_item("candy", 0.25, 3)
+puts new.total
+puts new.items
+new.void_last_transaction
+puts new.total
+puts new.items
+new.void_last_transaction
+puts new.total
+puts new.items
